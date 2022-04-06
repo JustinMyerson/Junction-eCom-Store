34852ba728dfe517ec20ace83865a716549474dc
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-06 14:13:04 +0200

    Create method to fetch price and convert to integer

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function convertPriceToInteger() {
 let loadMoreButton = document.getElementById("load-more-button");
 let addToCartButtons = document.getElementsByClassName("product-cart-button");
 let numberOfItemsInCart = document.getElementById("items-in-cart")
+let oldPrice = document.getElementsByClassName("old-price");
+let currentPrice = document.getElementsByClassName("current-price");
 
 if (loadMoreButton) {
   loadMoreButton.addEventListener("click", () => {
@@ -28,6 +30,13 @@ Array.from(document.getElementsByClassName("product-cart-button")).forEach(funct
     numberOfItemsInCart.innerHTML = itemsInCart.toString();
     console.log("Item added to cart");
   })
+})
+
+Array.from(document.getElementsByClassName("old-price")).forEach(function(oldPrice) {
+  let unformattedPrice = oldPrice.innerHTML
+  let formattedPrice = 0;
+  formattedPrice = parseInt(unformattedPrice.trim().substring(1).replace(/,/g, '').replace(/ /g,''))
+  console.log(formattedPrice)
 })
 
 
