3c6e2f923f81253c748900c91fe0d44e1e94c040
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-06 13:54:11 +0200

    Add to cart buttons all work

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+// Function
+function calculateDiscountPercentage (originalPrice, discountPrice) {
+  let discount = 100 * (originalPrice - discountPrice) / originalPrice
+  return Math.round(discount)
+}
+
+function convertPriceToInteger() {
+  let price = document.getElementById("")
+}
+
 // Header
 // Product
 
 let loadMoreButton = document.getElementById("load-more-button");
-let addToCartButton = document.getElementById("add-to-cart");
+let addToCartButtons = document.getElementsByClassName("product-cart-button");
 let numberOfItemsInCart = document.getElementById("items-in-cart")
 
 if (loadMoreButton) {
@@ -11,13 +21,16 @@ if (loadMoreButton) {
   });
 }
 
-if (addToCartButton) {
-  let items = 0;
-  addToCartButton.addEventListener("click", () => {
+let items = 0;
+for (let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click", () => {
     items += 1;
-    numberOfItemsInCart.innerHTML = items.toString()
+    numberOfItemsInCart.innerHTML = items.toString();
     console.log("Item added to cart");
-  });
+  })
 }
+
+
+console.log(calculateDiscountPercentage(19529, 17390))
 
 // Footer
