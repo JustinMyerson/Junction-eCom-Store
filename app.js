7d83e52b677bbf8422b7b67441025a38a634f211
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-06 14:56:13 +0200

    Add JavaDocs to functions

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-// Function
+/**
+ * Function that accesses an old price HTML element, and converts into an integer to be used when calculating
+ * the percentage discount that has been applied
+ * @param {Integer} originalPrice - The original price of the product
+ * @param {Integer} discountPrice - The discounted price of the product
+ * @return {Integer} Rounded percentage discount that has been applied
+ *
+ * @example
+ *
+ * calculateDiscountPercentage(100, 90)
+ */
+
 function calculateDiscountPercentage(originalPrice, discountPrice) {
   let discount = (100 * (originalPrice - discountPrice)) / originalPrice;
   return Math.round(discount);
@@ -34,6 +45,12 @@ Array.from(document.getElementsByClassName("product-cart-button")).forEach(
   }
 );
 
+/**
+ * Function that accesses an old price HTML element, and converts into an integer to be used when calculating
+ * the percentage discount that has been applied
+ * @return {Integer []} Array of old product prices that have not been discounted yet
+ */
+
 function getOldPrices() {
   let priceArray = new Array();
   Array.from(document.getElementsByClassName("old-price")).forEach(function (
@@ -49,6 +66,12 @@ function getOldPrices() {
   });
   return priceArray;
 }
+
+/**
+ * Function that accesses the current price HTML element, and converts into an integer to be used when calculating
+ * the percentage discount that has been applied
+ * @return {Integer []} Array of old product prices that have been discounted
+ */
 
 function getCurrentPrices() {
   let priceArray = new Array();
@@ -68,10 +91,26 @@ function getCurrentPrices() {
 
 const oldPriceArray = getOldPrices();
 const currentPriceArray = getCurrentPrices();
-for (let i = 0; i < 3; i++) {
-  console.log(
-    calculateDiscountPercentage(oldPriceArray[i], currentPriceArray[i])
-  );
+
+/**
+ * Function that accesses an old price HTML element, and converts into an integer to be used when calculating
+ * the percentage discount that has been applied
+ * @param {Integer} oldPriceArray - The original prices of the products
+ * @param {Integer} currentPriceArray - The discounted prices of the products
+ *
+ * @example
+ *
+ * calculateDiscounts(oldPrices, currentPrices)
+ */
+
+function calculateDiscounts(oldPriceArray, currentPriceArray) {
+  for (let i = 0; i < 3; i++) {
+    console.log(
+      `${calculateDiscountPercentage(oldPriceArray[i], currentPriceArray[i])}%`
+    );
+  }
 }
+
+calculateDiscounts(oldPriceArray, currentPriceArray);
 
 // Footer
