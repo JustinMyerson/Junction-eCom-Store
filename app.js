d233dd85da469a41acbb10aead7f29bc65af4f0d
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-06 13:17:44 +0200

    Change number of items in cart on button click

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 // Product
 
 let loadMoreButton = document.getElementById("load-more-button");
+let addToCartButton = document.getElementById("add-to-cart");
+let numberOfItemsInCart = document.getElementById("items-in-cart")
 
 if (loadMoreButton) {
-  console.log("found")
   loadMoreButton.addEventListener("click", () => {
     console.log("Show more button clicked");
+  });
+}
+
+if (addToCartButton) {
+  let items = 0;
+  addToCartButton.addEventListener("click", () => {
+    items += 1;
+    numberOfItemsInCart.innerHTML = items.toString()
+    console.log("Item added to cart");
   });
 }
 
