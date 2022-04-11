0de142f3ee8cac2ccdcdf6951cd3d27a03235bcf
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-11 13:04:02 +0200

    Add all elements that need to be rendered by generate product card

--- generateProductCard.js
+++ generateProductCard.js
@@ -6,7 +6,6 @@
  */
 
 function generateProductCard(product) {
-  console.log(product);
   const listItem = document.createElement("li");
   const productDiv = document.createElement("div");
   const productCard = document.createElement("div");
@@ -17,23 +16,54 @@ function generateProductCard(product) {
   const productDetails = document.createElement("div");
   const productName = document.createElement("h2");
   const productDesigner = document.createElement("h3");
+  const strongDesigner = document.createElement("strong");
   const productDescription = document.createElement("div");
   const pricesAndIcons = document.createElement("div");
   const prices = document.createElement("div");
   const oldPrices = document.createElement("div");
   const currentPrices = document.createElement("div");
-  const productCartButton = document.createElement("button");
-  const cartButton = document.createElement("img");
+  const productCartButton = document.createElement("div");
+  const cartButton = document.createElement("button");
+  const buttonImage = document.createElement("img");
 
   listItem.classList("product-list-item");
-  document.appendChild(listItem);
   productDiv.classList("product-div");
-  document.appendChild(productDiv);
   productDiv.classList("product-card");
-  document.appendChild(productCard);
   singleProduct.href("single-product.html");
   productImageAsset.classList("product-image-asset");
+  productImageDiv.classList("product-image");
+  productImageText.classList("product-image-text");
+  productImageText.id("discount");
+  productDetails.classList("product-details");
+  productName.classList("product-name");
+  productDesigner.classList("product-designer");
+  strongDesigner.classList("strong-designer");
+  productDescription.classList("product-description");
+  pricesAndIcons.classList("prices-and-icons");
+  prices.classList("prices");
+  oldPrices.classList("old-price");
+  currentPrices.classList("current-price");
+  productCartButton.classList("product-cart-button");
+  cartButton.id("add-to-cart");
+  buttonImage.classList("cart-image");
+
+  document.appendChild(listItem);
+  document.appendChild(productDiv);
+  document.appendChild(productCard);
   document.appendChild(productImageAsset);
+  document.appendChild(productImageDiv);
+  document.appendChild(productImageText);
+  document.appendChild(productName);
+  document.appendChild(productDesigner);
+  document.appendChild(strongDesigner);
+  document.appendChild(productDescription);
+  document.appendChild(pricesAndIcons);
+  document.appendChild(prices);
+  document.appendChild(oldPrices);
+  document.appendChild(currentPrices);
+  document.appendChild(productCartButton);
+  document.appendChild(cartButton);
+  document.appendChild(buttonImage);
 
   return listItem;
 }
