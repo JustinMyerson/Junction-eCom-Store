b3689ebaed88f4c62c45b3f4b9003d49a9e3a155
JustinMyerson <100346942+JustinMyerson@users.noreply.github.com>
2022-04-11 10:58:14 +0200

    Set up Display Products

---
+++ displayProducts.js
@@ -0,0 +1,14 @@
+/**
+ * Function that displays the product cards for us
+ * @param {json} products
+ *
+ * @example
+ * displayProducts(listOfProducts)
+ */
+
+function displayProducts(products) {
+  const displayContainer = document.getElementById("display-container");
+  displayContainer.textContent = "";
+}
+
+export { displayProducts };
--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import { API_URL } from "./shared.js";
 import { doFetch } from "./utils.js";
+import { displayProducts } from "./displayProducts.js";
 
-function main() {
+async function main() {
   const products = await doFetch(API_URL);
-  console.log(products)
+  console.log(products);
 }
 
 main();
